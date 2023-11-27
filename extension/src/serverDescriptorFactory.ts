1eca330c6d308c3d2bab976de1a4448a7f88ae34
Jason Paulos <jasonpaulos@users.noreply.github.com>
2023-11-27 11:19:15 -0500

    Use node server in extension server factory

--- extension/src/serverDescriptorFactory.ts
+++ extension/src/serverDescriptorFactory.ts
@@ -1,29 +1,35 @@
-import * as Net from 'net';
 import * as vscode from 'vscode';
 import { ProviderResult } from 'vscode';
-import { AvmDebugSession } from '../../src/common';
+import { Server as DebugAdapterServer } from '../../src/node';
 import { workspaceFileAccessor } from './fileAccessor';
 
 export class ServerDebugAdapterFactory
   implements vscode.DebugAdapterDescriptorFactory
 {
-  private server?: Net.Server;
+  private server?: DebugAdapterServer;
 
   createDebugAdapterDescriptor(
     _session: vscode.DebugSession,
     _executable: vscode.DebugAdapterExecutable | undefined,
   ): ProviderResult<vscode.DebugAdapterDescriptor> {
     if (!this.server) {
-      this.server = Net.createServer((socket) => {
-        const session = new AvmDebugSession(workspaceFileAccessor);
-        session.setRunAsServer(true);
-        session.start(socket as NodeJS.ReadableStream, socket);
-      }).listen(0);
+      this.server = new DebugAdapterServer({
+        fileAccessor: workspaceFileAccessor,
+        port: 0,
+        onSocketError: (err) => {
+          vscode.window.showErrorMessage(
+            `Debugger adapter socket error: ${err.message}`,
+          );
+        },
+        onServerError: (err) => {
+          vscode.window.showErrorMessage(
+            `Debugger adapter server error: ${err.message}`,
+          );
+        },
+      });
     }
 
-    return new vscode.DebugAdapterServer(
-      (this.server.address() as Net.AddressInfo).port,
-    );
+    return new vscode.DebugAdapterServer(this.server.port());
   }
 
   dispose() {
